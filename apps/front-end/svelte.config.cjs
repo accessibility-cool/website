7501f71fef85de6cfa0a748ef0d17221a03dbaa4
Simon Phumin Schweikert <git@simonschweikert.com>
2024-01-27 22:37:30 +0100

    added sveltekit & playwright config

--- apps/front-end/svelte.config.cjs
+++ apps/front-end/svelte.config.cjs
@@ -1,7 +1,14 @@
+import adapter from '@sveltejs/adapter-netlify';
 const sveltePreprocess = require('svelte-preprocess');
 
 module.exports = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess(),
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: sveltePreprocess()
+};
+
+export default {
+    kit: {
+        adapter: adapter()
+    }
 };
