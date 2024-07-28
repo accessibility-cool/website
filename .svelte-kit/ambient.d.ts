4193d57f143b413fe4a518b10ade7b85de1c6111
Simon Phumin Schweikert <git@simonschweikert.com>
2024-07-28 19:45:48 +0200

    dependencies upgraded and tsconfig updated

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -74,12 +74,15 @@ declare module '$env/static/private' {
 	export const VSCODE_IPC_HOOK_CLI: string;
 	export const VSCODE_L10N_BUNDLE_LOCATION: string;
 	export const WALLABY_PRODUCTION: string;
+	export const NODE_ENV: string;
+	export const __NEXT_PRIVATE_STANDALONE_CONFIG: string;
+	export const PORT: string;
+	export const __NEXT_PRIVATE_ORIGIN: string;
 	export const NX_WORKSPACE_ROOT_PATH: string;
 	export const ELECTRON_NO_ASAR: string;
 	export const CI: string;
 	export const NX_PROJECT_GLOB_CACHE: string;
 	export const NX_CACHE_PROJECTS_CONFIG: string;
-	export const NODE_ENV: string;
 }
 
 /**
@@ -96,9 +99,11 @@ declare module '$env/static/public' {
 }
 
 /**
- * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/master/packages/adapter-node) (or running [`vite preview`](https://kit.svelte.dev/docs/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://kit.svelte.dev/docs/configuration#env) (if configured).
+ * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/main/packages/adapter-node) (or running [`vite preview`](https://kit.svelte.dev/docs/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://kit.svelte.dev/docs/configuration#env) (if configured).
  * 
  * This module cannot be imported into client-side code.
+ * 
+ * Dynamic environment variables cannot be used during prerendering.
  * 
  * ```ts
  * import { env } from '$env/dynamic/private';
@@ -157,12 +162,15 @@ declare module '$env/dynamic/private' {
 		VSCODE_IPC_HOOK_CLI: string;
 		VSCODE_L10N_BUNDLE_LOCATION: string;
 		WALLABY_PRODUCTION: string;
+		NODE_ENV: string;
+		__NEXT_PRIVATE_STANDALONE_CONFIG: string;
+		PORT: string;
+		__NEXT_PRIVATE_ORIGIN: string;
 		NX_WORKSPACE_ROOT_PATH: string;
 		ELECTRON_NO_ASAR: string;
 		CI: string;
 		NX_PROJECT_GLOB_CACHE: string;
 		NX_CACHE_PROJECTS_CONFIG: string;
-		NODE_ENV: string;
 		[key: `PUBLIC_${string}`]: undefined;
 		[key: `${string}`]: string | undefined;
 	}
@@ -172,6 +180,8 @@ declare module '$env/dynamic/private' {
  * Similar to [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), but only includes variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
  * 
  * Note that public dynamic environment variables must all be sent from the server to the client, causing larger network requests — when possible, use `$env/static/public` instead.
+ * 
+ * Dynamic environment variables cannot be used during prerendering.
  * 
  * ```ts
  * import { env } from '$env/dynamic/public';
